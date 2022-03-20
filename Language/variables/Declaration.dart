7a326b53faee0a3b9d0d9b8948391cc6598bc5d9
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-21 00:22:18 +0600

    initial commit

---
+++ Language/variables/Declaration.dart
@@ -0,0 +1,10 @@
+void main() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['satrun'],
+    'url': './assets/img/ANSI_ISO_C++_WP.jpg'
+  };
+}
